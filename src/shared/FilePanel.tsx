28e7e909b92db72eaecb18ed7d1b2d539f22089e
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2024-12-29 15:52:20 +0000

    Update FilePanel Component

--- src/shared/FilePanel.tsx
+++ src/shared/FilePanel.tsx
@@ -2,8 +2,11 @@ import { ReactElement } from "react";
 import { useCanvas } from "../CanvasContext";
 import { getCanvasImage } from "../utils/canvasUtils";
 import { saveAs } from "file-saver";
+import { useDispatch } from "react-redux";
+import { show } from "../modules/modals/slice";
 
 export const FilePanel = (): ReactElement => {
+  const dispatch = useDispatch();
   const canvasRef = useCanvas();
 
   const exportToFile = async (): Promise<void> => {
@@ -24,6 +27,18 @@ export const FilePanel = (): ReactElement => {
         <div className="field-row">
           <button className="save-button" onClick={exportToFile}>
             Export
+          </button>
+          <button
+            className="save-button"
+            onClick={() => dispatch(show("PROJECTS_SAVE_MODAL"))}
+          >
+            Save
+          </button>
+          <button
+            className="load-button"
+            onClick={() => dispatch(show("PROJECTS_MODAL"))}
+          >
+            Load
           </button>
         </div>
       </div>
