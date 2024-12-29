60a29145bd65809c7e792cd74c5d3832955a0db6
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2024-12-29 15:53:22 +0000

    Add ModalLayer component

---
+++ src/ModalLayer.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from "react";
+import { useSelector } from "react-redux";
+import { modalNameSelector } from "./modules/modals/slice";
+import ProjectsModal from "./ProjectsModal";
+import ProjectSaveModal from "./ProjectSaveModal";
+
+export default function ModalLayer(): ReactElement | null {
+  const modalName = useSelector(modalNameSelector);
+
+  switch (modalName) {
+    case "PROJECTS_MODAL": {
+      return <ProjectsModal />;
+    }
+    case "PROJECTS_SAVE_MODAL": {
+      return <ProjectSaveModal />;
+    }
+    default:
+      return null;
+  }
+}
