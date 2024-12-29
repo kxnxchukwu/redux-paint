0b5c56540688df54dfe648826d4ad844b1023edc
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2024-12-29 15:56:07 +0000

    Update store to redux toolkit

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,26 @@
-import { legacy_createStore as createStore, combineReducers } from "redux";
-import { reducer as strokes } from "./modules/strokes/reducer";
-import { reducer as currentStroke } from "./modules/currentStroke/reducer";
-import { reducer as historyIndex } from "./modules/historyIndex/reducer";
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+import { logger } from "redux-logger";
+import strokes from "./modules/strokes/slice";
+import { currentStroke } from "./modules/currentStroke/slice";
+import { modalVisible } from "./modules/modals/slice";
+import historyIndex from "./modules/historyIndex/slice";
+import { projectsList } from "./modules/projectsList/slice";
+import { RootState } from "./utils/types";
+import { useDispatch } from "react-redux";
 
-export const store = createStore(
-  combineReducers({
+export const store = configureStore({
+  reducer: {
     strokes,
     currentStroke,
     historyIndex,
-  })
-);
+    modalVisible,
+    projectsList,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
